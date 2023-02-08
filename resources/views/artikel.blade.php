c61eadc5dc5f0a114bcbdb52eb08c6b059c0fd83
Zhofran27 <Zhofran275@gmail.com>
2023-02-08 09:38:08 +0700

    membuat navbar

---
+++ resources/views/artikel.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+<h1>HALAMAN ARTIKEL</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('artikel', [
+        "title" => "artikel"
+    ]);
+});
+
+Route::get('/kategori', function () {
+    return view('kategori');
 });
